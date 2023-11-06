6f49cd647589ec05fbc2027271f6f2c6550813a1
Anthony Fort <ajf2195@columbia.edu>
2023-11-06 16:26:58 +0000

    Converts tabula data to dataframe and sets column headings

--- service_scraper.py
+++ service_scraper.py
@@ -1,27 +1,75 @@
+import pandas as pd
 import tabula
+
 pdf_path = "https://asms.uk/wp-content/uploads/2023/11/music-list_2023-11-01_2023-11-30.pdf"
 pdf_path_2 = "https://asms.uk/wp-content/uploads/2023/08/music-list_2023-09-01_2023-09-30.pdf"
 
-# DFS stands for dataframes
-dfs = tabula.read_pdf(pdf_path, pages="all", stream=True)
-dfs_2 = tabula.read_pdf(pdf_path_2, pages="all", stream=True)
+# Stores pdf info in dataframe
+df1 = pd.DataFrame(tabula.read_pdf(pdf_path, pages="all", stream=True)[0])
+df2 = pd.DataFrame(tabula.read_pdf(pdf_path_2, pages="all", stream=True)[0])
 
-anthems = []
+dataframes = [
+  df1,
+  df2
+]
 
-for x in range(len(dfs_2[0])):
-  if 'Anthem' in str(list((dfs_2[0].loc[x]))):
-    item = list((dfs_2[0].loc[x]))
-    # print(item)
-    anthems.append(item)
+# Set column names
+for df in dataframes:
+  df.columns.values[0] = "music_item"
+  df.columns.values[-1] = "readings"
 
-for item in anthems:
-  new_item_array = item[-1].split('; ')
-  anthems[anthems.index(item)][-1] = new_item_array    
+cols1 = len(df1.axes[1])
+print(cols1)
 
-print(anthems)    
+cols2 = len(df2.axes[1])
+print(cols2)
+
+# TODO make df1 and df2 have the same number of columns (one for music and one for readings)
+
+
+# pdfs = [
+#   dfs,
+#   dfs_2
+# ]
+
+# combined_pdfs = []
+
+# for pdf in pdfs:
+#   combined_pdfs += pdf
+
+# for pdf in combined_pdfs:
+#   print(pdf)
+
 
 
 # anthems = []
+
+# for pdf in pdfs:
+#   for x in range(len(pdf[0])):
+#     if 'Anthem' in str(list((dfs_2[0].loc[x]))):
+#      item = list((dfs_2[0].loc[x]))
+#      anthems.append(item)
+
+# print(anthems[5])
+
+# for item in anthems:
+#   item_index = anthems.index(item)
+#   print(item_index)
+  # print(item[-1], type(item[-1]))
+  # related_readings_list = item[-1].split('; ')
+  # print(related_readings_list)
+
+  # for item in anthems:
+  #   reading_list = item[-1].split('; ')
+  #   print(reading_list)
+  #   print(anthems[anthems.index(item)][-1])
+  #   anthems[anthems.index(item)][-1] = reading_list  
+  #   print(anthems[anthems.index(item)][-1])
+
+# print(anthems)    
+
+
+
 
 # for x in range(len(dfs[0])):
 #   if 'Anthem' in str(list((dfs[0].loc[x]))):
@@ -30,10 +78,8 @@ print(anthems)
     
 
 # for item in anthems:
-#   new_item_array = item[1].split('; ')
-#   anthems[anthems.index(item)][1] = new_item_array
+#   reading_list = item[-1].split('; ')
+#   anthems[anthems.index(item)][-1] = reading_list
  
 
 # print(anthems)
-
-# print(dfs_2)
