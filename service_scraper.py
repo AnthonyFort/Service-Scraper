7ea5cf8c27ec589214a5e2ea92378f08efcee5b7
Anthony Fort <ajf2195@columbia.edu>
2023-11-06 15:06:22 +0000

    Experiments with new pdf and indexes from back

--- service_scraper.py
+++ service_scraper.py
@@ -1,21 +1,39 @@
 import tabula
 pdf_path = "https://asms.uk/wp-content/uploads/2023/11/music-list_2023-11-01_2023-11-30.pdf"
+pdf_path_2 = "https://asms.uk/wp-content/uploads/2023/08/music-list_2023-09-01_2023-09-30.pdf"
 
 # DFS stands for dataframes
 dfs = tabula.read_pdf(pdf_path, pages="all", stream=True)
-# print(list((dfs[0].loc[0])))
+dfs_2 = tabula.read_pdf(pdf_path_2, pages="all", stream=True)
 
 anthems = []
 
-for x in range(len(dfs[0])):
-  if 'Anthem' in str(list((dfs[0].loc[x]))):
-    item = list((dfs[0].loc[x]))
+for x in range(len(dfs_2[0])):
+  if 'Anthem' in str(list((dfs_2[0].loc[x]))):
+    item = list((dfs_2[0].loc[x]))
+    # print(item)
     anthems.append(item)
-    
 
 for item in anthems:
-  new_item_array = item[1].split('; ')
-  anthems[anthems.index(item)][1] = new_item_array
+  new_item_array = item[-1].split('; ')
+  anthems[anthems.index(item)][-1] = new_item_array    
+
+print(anthems)    
+
+
+# anthems = []
+
+# for x in range(len(dfs[0])):
+#   if 'Anthem' in str(list((dfs[0].loc[x]))):
+#     item = list((dfs[0].loc[x]))
+#     anthems.append(item)
+    
+
+# for item in anthems:
+#   new_item_array = item[1].split('; ')
+#   anthems[anthems.index(item)][1] = new_item_array
  
 
-print(anthems)
+# print(anthems)
+
+# print(dfs_2)
